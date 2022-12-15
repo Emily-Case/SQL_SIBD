3f3546850fa87752c69d2baa829c1eff6edce575
Emilyyy <95703134+gayAssEmily@users.noreply.github.com>
2022-12-15 11:37:18 +0000

    Update pkg_loja.sql

--- Projeto4/pkg_loja.sql
+++ Projeto4/pkg_loja.sql
@@ -1,40 +1,42 @@
+@@tabelas.sql
 CREATE SEQUENCE seq_fatura_ordem
     START WITH 0
     INCREMENT BY 1
     MINVALUE 0
     MAXVALUE 9999
     NOCYCLE;
-
-@tabelas.sql
-@pkg_loja.pks
-@pkg_loja.pkb
-
+@@pkg_loja.pks
+@@pkg_loja.pkb
+-- ----------------------------------------------------------------------------
 VARIABLE numFatura1 NUMBER;
 VARIABLE numFatura2 NUMBER;
 VARIABLE numFatura3 NUMBER;
+VARIABLE numFatura4 NUMBER;
 VARIABLE delCompra1 NUMBER;
 VARIABLE delCompra2 NUMBER;
-VARIABLE delCompra3 NUMBER;
-
+-- ----------------------------------------------------------------------------
 BEGIN
 pkg_loja.regista_cliente(111111111,'Joaquim','M',2000,'Lisboa');
 pkg_loja.regista_cliente(222222222,'Beatriz','F',2003,'Lisboa');
-pkg_loja.regista_produto(1234567891234,'Maça','Comida',2.99,1000);
+pkg_loja.regista_produto(1234567891234,'Maca','Comida',2.99,1000);
 pkg_loja.regista_produto(2345678912345,'Massa','Comida',0.99,500);
 pkg_loja.regista_produto(3456789123456,'Carne','Comida',4.99,2000);
-:numFatura1 := pkg_loja.regista_compra(111111111, 1234567891234, 5);
-:numFatura2 := pkg_loja.regista_compra(111111111, 2345678912345, 15, :numFatura1);
-:numFatura2 := pkg_loja.regista_compra(111111111, 3456789123456, 10, :numFatura1);
-:numFatura3 := pkg_loja.regista_compra(222222222, 2345678912345, 20);
-:delCompra1 := pkg_loja.remove_compra(:numFatura2, 2345678912345);
-:delCompra2 := pkg_loja.remove_compra(:numFatura2, 1234567891234);
-:delCompra3 := pkg_loja.remove_compra(:numFatura3, 2345678912345);
+:numFatura1 := pkg_loja.regista_compra(111111111,1234567891234,5);
+:numFatura2 := pkg_loja.regista_compra(111111111,2345678912345,15,:numFatura1);
+:numFatura2 := pkg_loja.regista_compra(111111111,3456789123456,10,:numFatura1);
+:numFatura3 := pkg_loja.regista_compra(222222222,2345678912345,20);
+:numFatura4 := pkg_loja.regista_compra(222222222,3456789123456,100,:numFatura3);
+:delCompra1 := pkg_loja.remove_compra(:numFatura1,3456789123456);
+:delCompra2 := pkg_loja.remove_compra(:numFatura3);
 END;
 /
+-- ----------------------------------------------------------------------------
 SELECT * FROM cliente;
 SELECT * FROM produto;
 SELECT * FROM fatura;
 SELECT * FROM linhafatura;
+PRINT numFatura1;
+PRINT numFatura3;
+PRINT delCompra1;
 PRINT delCompra2;
-PRINT delCompra3;
-DROP SEQUENCE seq_fatura_ordem;
+-- ----------------------------------------------------------------------------
